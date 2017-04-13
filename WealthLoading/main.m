990d671b4107ce1cf2225eed0f0da058959f50d9
liwei <fanxiaobin@liweideMacBook-Air.local>
2017-04-13 16:52:41 +0800

    Initial Commit

---
+++ WealthLoading/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WealthLoading
+//
+//  Created by fanxiaobin on 2017/4/13.
+//  Copyright © 2017年 fanxiaobin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
